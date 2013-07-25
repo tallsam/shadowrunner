b883717e510762a0759f4038c8378ebf5441fb2b
Neil Steventon <neil.steventon@gmail.com>
2013-07-25 22:45:21 +0800

    retesting framework alterd main load page - ondeviceready is only ever run once when the framework is initialised. map pointer now shows location of phone

--- EclipseWorkspace/ShadowRunner/src/com/shadowrunner/MainShadowRunner.java
+++ EclipseWorkspace/ShadowRunner/src/com/shadowrunner/MainShadowRunner.java
@@ -10,7 +10,7 @@ public class MainShadowRunner extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/html/index.html");
+        super.loadUrl("file:///android_asset/www/html/shadowmap.html");
     }
     
 }
